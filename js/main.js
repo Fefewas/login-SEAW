9fdfa81038da8f0d2f73382203efde08daf88a46
Frederick <maussafrederick18@gmail.com>
2023-05-04 04:04:18 -0500

    Frontend last update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-const mostrar = document.querySelectorAll('i');
+let strength = document.getElementById('strength-password');
+let password = document.getElementById('password');
+let confirmPassword = document.getElementById('confirm-password');
 const inputs = document.querySelectorAll('input');
+
+
+//código encargado de hacer el cambio de estado del placeholder de los inputs tipo password y text.
 inputs.forEach( input =>{
     input.onfocus = function(){
         input.previousElementSibling.classList.add('top');
@@ -14,28 +19,65 @@ inputs.forEach( input =>{
         input.parentNode.classList.remove('focus');
     }
 });
-function mostrarContraseña(){
-    var visible = document.getElementById('contraseña');
+
+// función encargada de evaluar la fortaleza de la contraseña ingresada por el usuario.
+password.addEventListener('keyup', passwordStrength);
+function passwordStrength() {
+    let strengthBar = document.getElementById('strength-password-bar');
+    let strengthText = document.getElementById('strength-password-text');
+    strength.classList.remove('invisible');
+    if (password.value.length > 12) {
+        strengthBar.classList.remove('fill--110');
+        strengthBar.classList.remove('fill--25');
+        strengthBar.classList.remove('fill--60');
+        strengthBar.classList.add('fill--110');
+        strengthText.innerHTML = 'El número máximo de caracteres es de 12';
+    } else if (password.value.length <= 4) {
+        strengthBar.classList.remove('fill--110');
+        strengthBar.classList.remove('fill--100');
+        strengthBar.classList.remove('fill--60');
+        strengthBar.classList.add('fill--25');
+        strengthText.innerHTML = 'Débil';
+    } else if (password.value.length <= 7) {
+        strengthBar.classList.remove('fill--110');
+        strengthBar.classList.remove('fill--25');
+        strengthBar.classList.remove('fill--100');
+        strengthBar.classList.add('fill--60');
+        strengthText.innerHTML = 'Medio';
+    } else {
+        strengthBar.classList.remove('fill--110');
+        strengthBar.classList.remove('fill--25');
+        strengthBar.classList.remove('fill--60');
+        strengthBar.classList.add('fill--100');
+        strengthText.innerHTML = '¡Fuerte!';
+    }
+    if (password.value.length = ''){
+        strength.classList.add('invisible');
+    }
+}
+
+// función encargada de hacer el cambio de la visibilidad de la contraseña.
+function showPassword(){
+    var visible = document.getElementById('password');
+    var confirmVisible = document.getElementById('confirm-password');
     if(visible.type == 'password'){
         visible.type = 'text';
+        confirmVisible.type = 'text';
     }
     else{
         visible.type = 'password';
+        confirmVisible.type = 'password';
     }
 }
-if (mostrar.classList.contains('fa-eye-slash')){
-    mostrar.forEach( i =>{
-        i.onclick = function(){
-            mostrar.classList.remove('fa-eye-slash');
-            mostrar.classList.add('fa-eye');
-        }
-    });
-}
-else{
-    mostrar.forEach( i =>{
-        i.onclick = function(){
-            mostrar.classList.remove('fa-eye');
-            mostrar.classList.add('fa-eye-slash');
-        }
-    });
+
+// función encargada de asegurarse de que el usuario esté escribiendo correctamente la contraseña en el input de "confirmar contraseña".
+confirmPassword.addEventListener('keyup', samePassword);
+function samePassword(){
+    let samePasswordText = document.getElementById('same-password-text');
+    if(password.value != confirmPassword.value){
+        samePasswordText.innerHTML = "Las contraseñas no coinciden";
+    }
+    else{
+        samePasswordText.innerHTML = "";
+    }
 }
